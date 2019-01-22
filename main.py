7e9d9cc015af20d14139a8093fb74728956b7070
SouravMzdr <32381641+SouravMzdr@users.noreply.github.com>
2019-01-22 17:48:14 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from bs4 import BeautifulSoup
 from requests import get
 
+#Setting up the url
 search=input("Enter search term")
 url="http://www.wikipedia.org/wiki/"+search
 htmlString =get(url).text
+
+#Parsing the soup
 html=BeautifulSoup(htmlString,'lxml')
 entries=html.find_all('div', {"class":"mw-parser-output"})
 text=[e.get_text() for e in entries]
-print(text)
 
+#Put the extracted text into seperate txt file
 path=search+".txt"
-f=open(path,"w")
+f=open(path,"w+")
 f.writelines(text[0])
 
 
