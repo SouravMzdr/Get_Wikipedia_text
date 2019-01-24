5f5da411a65c2d51d2c460dd5c6e3337df8562d0
SouravMzdr <32381641+SouravMzdr@users.noreply.github.com>
2019-01-24 11:09:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from bs4 import BeautifulSoup
 from requests import get
+import os
+import io
 
 #Setting up the url
-search=input("Enter search term:")
+search=input("Enter Wikipedia search term:")
 search=search.capitalize()
 search=search.replace(" ","_")
 
 url="http://www.wikipedia.org/wiki/"+search
 htmlString =get(url).text
 
+#Check for Error 404
+if get(url).status_code == 404:
+    print("Page not found!!")
+    print("Please check whether your search term is spelled correctly and whether a wiki page exist for the searched topic")
+    os.system('pause')
+
 #Parsing the soup
 html=BeautifulSoup(htmlString,'lxml')
+
 entries = html.find('div', id="content")
 entries = entries.find_all(['h1','h2', 'p'])
 text = [e.get_text() for e in entries]
@@ -19,7 +28,6 @@ text = [e.get_text() for e in entries]
 search=search.replace("_"," ")
 path=search+".txt"
 
-f=open(path,"w+")
-for t in text:
-    f.write(t+"\n")
-
+with io.open(path, "w+", encoding="utf-8") as f:
+    for t in text:
+        f.write(t + "\n")
